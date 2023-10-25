7ca898649ed28e392ba7842a1f09ea3cfb2057a8
yanuarfauzan <yanuarisnain017@gmail.com>
2023-10-25 09:41:39 +0700

    Menambahkan entity untuk penampungan data

---
+++ src/main/java/com/springmvc/demomvc/entity/Product.java
@@ -0,0 +1,52 @@
+package com.springmvc.demomvc.entity;
+
+public class Product {
+    private Long id;
+    private String code;
+    private String name;
+    private double price;
+
+    public Product(Long id, String code, String name, double price) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
