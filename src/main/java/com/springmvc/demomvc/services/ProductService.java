5373bc7c31379eb1b0827cfb68f65cb79a828f25
yanuarfauzan <yanuarisnain017@gmail.com>
2023-10-25 09:43:01 +0700

    service untuk HomeController

---
+++ src/main/java/com/springmvc/demomvc/services/ProductService.java
@@ -0,0 +1,37 @@
+package com.springmvc.demomvc.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.springmvc.demomvc.entity.Product;
+import com.springmvc.demomvc.utils.RandomNumber;
+
+@Service
+public class ProductService {
+    private static List<Product> products = new ArrayList<Product>() {
+        {
+            add(new Product(RandomNumber.getRandom(1000, 9999), "001", "Product 001", 1000.0));
+            add(new Product(RandomNumber.getRandom(1000, 9999), "002", "Product 002", 2000.0));
+            add(new Product(RandomNumber.getRandom(1000, 9999), "003", "Product 003", 3000.0));
+            add(new Product(RandomNumber.getRandom(1000, 9999), "004", "Product 004", 4000.0));
+            add(new Product(RandomNumber.getRandom(1000, 9999), "005", "Product 005", 5000.0));
+            add(new Product(RandomNumber.getRandom(1000, 9999), "006", "Product 005", 5000.0));
+        }
+    };
+
+    public List<Product> findAll() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        product.setId(RandomNumber.getRandom(1000, 9999));
+        products.add(product);
+    }
+
+    public void deleteById(long id) {
+        products.removeIf(p -> p.getId() == id);
+    }
+
+}
